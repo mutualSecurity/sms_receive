3263994e3f1f066160cc897838296a2d18cdc8f8
parkash067 <pk_bscs@yahoo.com>
2017-05-04 17:50:52 +0500

    changes in sms receive application

--- app.js
+++ app.js
@@ -37,26 +37,31 @@ function tConvert (time) {
 }
 
 /* Function for insertion of data into database */
-function dataInsert(record) {
+function dataInsert(record,messageIndex) {
     // This line breaks received sms into array
     record = record.split(' ');
-    //converts time from 24hr to 12hr
-    timeCon = tConvert(record[3].toString().trim());
-    //fetch record from db
-    var query_s = client.query("SELECT bank_code,branch_code,street FROM res_partner WHERE rf_id='"+record[1]+"'");
-    // Stream results back one row at a time
-    query_s.on('row',function (row) {
-        //row is the record of table
-        query_ins = client.query("INSERT INTO mutual_guard_tracking(bank_code,branch_code,address,visit_date,visit_time,device_no)values('"+row.bank_code+"','"+row.branch_code+"','"+row.street+"','"+record[2]+"','"+timeCon+"','"+record[0]+"')");
-        console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SMS Received>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
-            + "\n"+"Device ID:",record[0]
-            +"\n"+"Station ID:",record[1]
-            +"\n"+"Date:",record[2]
-            +"\n"+"Time:",record[3]+
-            "\nConverted Time: ",timeCon
-            +"\n\n"+"SMS has been logged into successfully");
+    if(record.length == 4) {
+        //converts time from 24hr to 12hr
+        timeCon = tConvert(record[3].toString().trim());
+        //fetch record from db
+        var query_s = client.query("SELECT bank_code,branch_code,street FROM res_partner WHERE rf_id='" + record[1] + "'");
+        // Stream results back one row at a time
+        query_s.on('row', function (row) {
+            //row is the record of table
+            query_ins = client.query("INSERT INTO mutual_guard_tracking(bank_code,branch_code,address,visit_date,visit_time,device_no)values('" + row.bank_code + "','" + row.branch_code + "','" + row.street + "','" + record[2] + "','" + timeCon + "','" + record[0] + "')");
+            console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SMS Received>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+                + "\n" + "Device ID:", record[0]
+                + "\n" + "Station ID:", record[1]
+                + "\n" + "Date:", record[2]
+                + "\n" + "Time:", record[3] +
+                "\nConverted Time: ", timeCon
+                + "\n\n" + "SMS has been logged into db successfully");
+            del(port,messageIndex);
+        });
+    }
+    else {
         del(port);
-    });
+    }
 }
 
 /* Function for checking that COM port is open or not */
@@ -69,35 +74,43 @@ function onOpen(err) {
 /* Function for receiving of sms from sim and modem both */
 function onDataReceived(data) {
     data = data.toString();
-    var arr = data.split(',');
-    try {
-    // the synchronous code that we want to catch thrown errors on
-        if (arr[4] !== undefined){
-            var narr = arr[4].split('\n');
-            if (narr[1] !== undefined){
-                dataInsert(narr[1]);
-            }
-        }
-    } catch (err) {
-        console.log(err)
+    //console.log(data);
+    var arr = data.split('\n');
+    //console.log(arr);
+    if (arr[0] == '\r' && arr.length >3)
+    {
+        //console.log(((arr[1].split(','))[0].split(':'))[1].trim());
+        dataInsert(arr[2],((arr[1].split(','))[0].split(':'))[1].trim())
     }
+    // try {
+    // // the synchronous code that we want to catch thrown errors on
+    //     if (arr[4] !== undefined){
+    //         var narr = arr[4].split('\n');
+    //         if (narr[1] !== undefined){
+    //             console.log(narr);
+    //             dataInsert(narr[1]);
+    //         }
+    //     }
+    // } catch (err) {
+    //     console.log(err)
+    // }
 }
 
 /* Function for reading of sms from sim and modem both */
 function read(serial) {
     serial.write("AT+CMGF=1");
     serial.write('\r');
-    serial.write("AT+CPMS=\"MT\"");
+    serial.write("AT+CPMS=\"SM\"");
     serial.write('\r');
-    serial.write("AT+CMGL=\"REC READ\"");
+    serial.write("AT+CMGL=\"ALL\"");
     serial.write('\r');
 }
 
 /* Function for deletion of sms after logged into database */
-function del(serial) {
+function del(serial,messageIndex) {
     serial.write("AT+CMGF=1");
     serial.write('\r');
-    serial.write("AT+CMGD=1,1");
+    serial.write("AT+CMGD="+messageIndex);
     serial.write('\r');
 }
 
