ba99ef18d0a0fed5f94f7c0fa096eaeea2f5c0fc
parkash067 <pk_bscs@yahoo.com>
2017-07-24 14:18:36 +0500

    changes in sms service

--- sim900.js
+++ sim900.js
@@ -1,7 +1,9 @@
 /**
  * Created by pk on 5/24/2017.
  */
+var moment = require('moment');
 var pg = require('pg');
+timeCon='';
 /* Database connection string */
 const connectionString = "postgres://odoo:odoo@localhost/mutual-erp-bank";
 const client = new pg.Client(connectionString);
@@ -32,30 +34,67 @@ function onDataReceived(data) {
     data=data.toString().split(' ');
     if(data.length==4){
         //console.log("Received Data:"+data);
-        dataInsert(data)
+        checkVisit(data)
     }
 }
 
-/* Function for insertion of data into database */
-function dataInsert(record) {
+function dataInsert(row,guard_visit_time,device_record,second_visit) {
+    if (second_visit==0){
+         query_ins = client.query("INSERT INTO mutual_guard_tracking" + "(bank_code,branch_code,address,city,visit_date,visit_time,force_code,device_no,card_no)" + "values('" + row.bank_code + "','" + row.branch_code + "','" + row.street +"','" + row.city + "','" + device_record[2] + "','" + timeCon + "','" + row.force_code +"','" + device_record[0] + "','" + device_record[1] + "')");
+         console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SMS Received>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+                + "\n" + "Device ID:", device_record[0]
+                + "\n" + "Station ID:", device_record[1]
+                + "\n" + "Date:", device_record[2]
+                + "\n" + "Time:", device_record[3] +
+                "\nConverted Time: ", guard_visit_time
+                + "\n\n" + "SMS has been logged into db successfully");
+    }
+    else {
+        query_ins=client.query("UPDATE public.mutual_guard_tracking SET visit_time_two='" + second_visit + "'"+"WHERE card_no='"+ device_record[1] + "'"+" AND archive_signal is null");
+
+    }
+
+}
+
+/* Function for checking visits for patrolling */
+function checkVisit(record) {
     //converts time from 24hr to 12hr
     timeCon = tConvert(record[3].toString().trim());
     //fetch record from db
-    var query_s = client.query("SELECT bank_code,branch_code,street,city,force_code FROM res_partner WHERE rf_id='" + record[1] + "'");
-    // Stream results back one row at a time
-    query_s.on('row', function (row) {
-        if(row){
-            //row is the record of table
-            query_ins = client.query("INSERT INTO mutual_guard_tracking(bank_code,branch_code,address,city,visit_date,visit_time,force_code,device_no)values('" + row.bank_code + "','" + row.branch_code + "','" + row.street +"','" + row.city + "','" + record[2] + "','" + timeCon + "','" + row.force_code +"','" + record[0] + "')");
-            console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SMS Received>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
-                + "\n" + "Device ID:", record[0]
-                + "\n" + "Station ID:", record[1]
-                + "\n" + "Date:", record[2]
-                + "\n" + "Time:", record[3] +
-                "\nConverted Time: ", timeCon
-                + "\n\n" + "SMS has been logged into db successfully");
+    var check_signal = client.query("SELECT * FROM mutual_guard_tracking WHERE card_no='"+ record[1] + "'"+"AND archive_signal is null AND visit_time_two is null");
+    query_s = client.query("SELECT bank_code,branch_code,street,city,force_code FROM res_partner WHERE rf_id='" + record[1] + "'");
+    check_signal.on('end', function(result) {
+        if(result.rowCount == 0){
+            query_s.on('row', function (row) {
+                dataInsert(row,timeCon,record,0)
+            });
+        }
+        else {
+            var first_visit = result.rows[0].visit_time;
+            var guard_visit=timeCon;
+            diff=''
+            if(first_visit.indexOf('PM')>1 && guard_visit.indexOf('AM')>1 ){
+                var startTime = moment(first_visit, "HH:mm a");
+                var endTime = moment(guard_visit, "HH:mm a");
+                var diff = moment(endTime.add(1,'days')).diff(startTime,'minutes');
+
+            }
+            else {
+                var startTime = moment(first_visit, "HH:mm a");
+                var endTime = moment(guard_visit, "HH:mm a");
+                var diff = moment(endTime).diff(startTime,'minutes');
+            }
+            if(diff>15){
+                query_s.on('row', function (row) {
+                dataInsert(row,first_visit,record,guard_visit)
+            });
+            }
+
         }
     });
+
+
+
 }
 
 /* Function for conversion of Time from 24hr to 12hr */
